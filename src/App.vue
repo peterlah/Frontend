edc8484b3a4173c42d7dbe119e2ea91142d3b3bc
peterlah <peterlah@naver.com>
2024-01-19 10:23:12 +0900

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+        id="app"
+        class="container"
+    >
+        <header class="header">
+            <h1>Vue.js Frontend Application</h1>
+        </header>
+        <div class="row">
+            <div class="col-md-8 mx-auto mt-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">
+                            Response from Java API Server:
+                        </h2>
+                        <p class="card-text">{{ message }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            message: "",
+        };
+    },
+    mounted() {
+        this.fetchMessage();
+    },
+    methods: {
+        async fetchMessage() {
+            const apiUrl = process.env.VUE_APP_API_URL + "/api/message";
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            this.message = data.message;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    text-align: center;
+    margin-top: 20px;
+}
+
+.header h1 {
+    color: #007bff;
+    margin-bottom: 40px;
+}
+
+.card {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-title {
+    color: #343a40;
+    margin-bottom: 15px;
+}
+
+.card-text {
+    color: #6c757d;
+    font-size: 18px;
 }
 </style>
